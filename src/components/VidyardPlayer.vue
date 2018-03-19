1edd633a634cf2d0adc6c12a58693cdfe45b67c8
Basil Khan <basil.khan@vidyard.com>
2018-03-18 22:23:51 -0400

    initial commit

---
+++ src/components/VidyardPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="vidyard-player" ref="vidyardPlayer">
+    Hello Basil's first Vue App
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VidyardPlayer',
+  mounted () {
+    let uuid = this.$route.params.uuid
+    let vidyardEmbedCode = document.createElement('script')
+    vidyardEmbedCode.type = 'text/javascript'
+    vidyardEmbedCode.id = `vidyard_embed_code_${uuid}`
+    vidyardEmbedCode.setAttribute('src', `//play.vidyard.com/${uuid}.js?v=3.1.1&type=inline`)
+    this.$refs.vidyardPlayer.appendChild(vidyardEmbedCode)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import VidyardPlayer from '@/components/VidyardPlayer'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/watch/:uuid',
+      name: 'VidyardPlayer',
+      component: VidyardPlayer
+    }
+  ]
+})
